27759211cb0bcc33ec1744f975817d9f1fad03b8
jaffob <jaffob77@gmail.com>
2016-04-08 21:02:52 -0700

    start implementing lasers

---
+++ src/Square_Laser.java
@@ -0,0 +1,18 @@
+
+public class Square_Laser extends Square_Wall {
+
+	private Direction laserDirection;
+	
+	public Square_Laser(Vector2D position, Integer properties) {
+		super(position, properties);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	protected void absorbPropertyVal(int properties) {
+		super.absorbPropertyVal(properties);
+		
+		
+	}
+
+}
